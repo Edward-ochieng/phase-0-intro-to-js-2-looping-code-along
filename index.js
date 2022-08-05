8695fc209249e67e2d7b9edd838d948a7673ef1e
Edward <edward.ochieng@student.moringaschool.com>
2022-08-05 14:48:10 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+const writeCards = (array = ["Guadalupe", "Ollie", "Aki"]) => {
+    const messageArray = [];
+    for(const a of array) {
+        messageArray.push(`Thank you, ${a}, for the wonderful surprise gift!`);
+    }
+    return messageArray;
+}
+
+const countDown = (num = 10) => {
+    for(let number = num; number >= 0; number--){
+        console.log(number);
+    }
+}
